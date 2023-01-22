9566d72a1db376f992ff46bf7932efe9365b3e1d
Alex Gilson <alex.gilsoncampillo@gmail.com>
2023-01-22 20:52:57 +0100

    Basic structure for VariableDumperTs

--- test/VariableDumperTs.cpp
+++ test/VariableDumperTs.cpp
@@ -1,6 +1,45 @@
 
+#include <filesystem>
+#include <cassert>
+#include "DumperContainer.h"
+
+using namespace VariableDumper;
+
+class VariableDumperTs
+{
+public:
+	virtual ~VariableDumperTs() {};
+	virtual void setup()
+	{
+		std::filesystem::create_directory(path_);
+	};
+	virtual void teardown()
+	{
+		std::filesystem::remove(path_);
+	}
+	virtual void run()
+	{
+		setup();
+		assert(runTest());
+		teardown();
+	}
+private:
+	virtual bool runTest() = 0;
+	const std::string path_ = VARIABLE_DUMPER_TEST_DATA_DIR;
+};
+
+class BasicOneThreadDumpVariableDumperTs : public VariableDumperTs
+{
+	bool runTest() override
+	{
+		return true;
+	}
+public:
+	virtual ~BasicOneThreadDumpVariableDumperTs() {};
+};
 
 int main()
 {
+	BasicOneThreadDumpVariableDumperTs{}.run();
 	return false;
 }
