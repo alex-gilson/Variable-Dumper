83aff13e8ceee145e50082450666b6da5b34366e
alex-gilson <alex.gilsoncampillo@gmail.com>
2023-01-25 13:11:25 +0100

    Add coments to the DumperContainer class

--- src/DumperContainer.h
+++ src/DumperContainer.h
@@ -29,26 +29,42 @@ namespace VariableDumper
 class DumperContainer
 {
 private:
+    // Constructor that takes in the path of the Dumper object for non multithreaded runtimes
     DumperContainer(std::string path);
+    // Pointer to the singleton instance of DumperContainer
     static DumperContainer* instance;
-    std::unordered_map<std::string,     std::unique_ptr<Dumper>> dumperFileNameMap_;
+    // Maps the file name of a Dumper object to the corresponding Dumper
+    std::unordered_map<std::string, std::unique_ptr<Dumper>> dumperFileNameMap_;
+    // Maps the thread ID of a thread to the path of all Dumpers assigned to that thread
     std::unordered_map<std::thread::id, std::string> pathMap_;
+    // Maps the thread ID of a thread to a set of file names of the Dumper objects created by that thread
     std::unordered_map<std::thread::id, std::set<std::string>> threadFileNamesMap_;
+    // Set of file names of all the Dumper objects created by all threads
     std::set<std::string> fileNamesSet_;
+    // Returns the path of the Dumper object associated with the current thread
     std::string& getPath();
 
+    // Mutex objects for synchronizing access to the container
     std::mutex createDumperContainerMutex_;
     std::mutex writeToDumperMapMutex_;
     std::mutex updatePathMutex_;
 
 public:
+    // Returns a pointer to the singleton instance of DumperContainer
     static DumperContainer* getDumperContainer(const std::string& path = "");
+    // Updates the path of the Dumper object created by the current thread
     void updatePath(const std::string& path);
+    // Creates a new Dumper object with the specified name, dump size and count max
     void createDumper(const std::string& name, int dumpSize = -1, int countMax = -1);
+    // Returns a pointer to the Dumper object with the specified name
     Dumper* getDumper(const std::string& name);
+    // Destroys all the Dumper objects associated with the current thread
     void destroyDumpers();
+    // Sets the precision of the Dumper object with the specified name and associated with the current thread
     void setDumperPrecision(const std::string& name, int precision);
+    // Sets the precision of all the Dumper objects created
     void setDumpersPrecision(int precision);
+    // Dumps the data buffer using the Dumper object with the specified name
     template<typename T>
     void dump(T& buf, const std::string& name)
     {
